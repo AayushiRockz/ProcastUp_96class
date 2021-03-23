fcc10a0213d95b30e09db24a5b353337fa7c2e01
AayushiRockz <aayushisomkuwar993@gmail.com>
2021-03-23 14:53:20 +0530

    fixed welcome screen

--- Screens/WelcomeScreen.js
+++ Screens/WelcomeScreen.js
@@ -1,29 +1,40 @@
-import React , {Component} from 'react';
-import {View,Text,TouchableOpacity, StyleSheet, Modal, ScrollView, KeyboardAvoidingView, TextInput, Alert, Touchable} from 'react-native';
-import AppHeader from '../Component/AppHeader';
-import MyHeader from '../Component/MyHeader';
-import {SafeAreaProvider} from 'react-native-safe-area-context';
+import React, { Component } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  Image,
+  Modal,
+  KeyboardAvoidingView,
+  StyleSheet,
+  TouchableOpacity,
+  Alert,
+  ScrollView
+} from "react-native";
 
-import firebase from 'firebase';
-import db from '../config';
+
+import db from "../config";
+import firebase from "firebase";
+
+import { Icon } from "react-native-elements";
+import { RFValue } from "react-native-responsive-fontsize";
 
 export default class WelcomeScreen extends Component {
+  constructor() {
+    super();
+    this.state = {
+      emailId: "",
+      password: "",
+      firstName: "",
+      lastName: "",
+      address: "",
+      contact: "",
+      confirmPassword: "",
+      isModalVisible: "false"
+    };
+  }
 
-    constructor() {
-        super();
-        this.state = {
-          emailId: "",
-          password: "",
-          firstName: "",
-          lastName: "",
-          address: "",
-          contact: "",
-          confirmPassword: "",
-          isModalVisible: "false"
-        };
-      }
-    
-  userSignUp =(emailId, password, confirmPassword)=>{
+  userSignUp = (emailId, password, confirmPassword) => {
     if (password !== confirmPassword) {
       return Alert.alert("password doesn't match\nCheck your password.");
     } else {
@@ -37,7 +48,7 @@ export default class WelcomeScreen extends Component {
             contact: this.state.contact,
             email_id: this.state.emailId,
             address: this.state.address,
-          
+            IsBookRequestActive: false
           });
           return Alert.alert("User Added Successfully", "", [
             {
@@ -69,7 +80,7 @@ export default class WelcomeScreen extends Component {
       });
   };
 
-  modal = () => {
+  showModal = () => {
     return (
       <Modal
         animationType="slide"
@@ -171,108 +182,146 @@ export default class WelcomeScreen extends Component {
             <TouchableOpacity
               style={styles.registerButton}
               onPress={() =>
-                this.userSignUp=()=>{
-                  (
-                    this.state.emailId,
-                    this.state.password,
-                    this.state.confirmPassword
-                  )
-                }
+                this.userSignUp(
+                  this.state.emailId,
+                  this.state.password,
+                  this.state.confirmPassword
+                )
               }
             >
               <Text style={styles.registerButtonText}>Register</Text>
             </TouchableOpacity>
-            <TouchableOpacity
+            <Text
               style={styles.cancelButtonText}
               onPress={() => {
                 this.setState({ isModalVisible: false });
               }}
             >
               Cancel
-            </TouchableOpacity>
+            </Text>
           </View>
         </ScrollView>
       </Modal>
     );
   };
-    render(){
-        return(
+  render() {
+    return (
+      <View style={styles.container}>
+        {this.showModal()}
+        <View style={{ flex: 0.25 }}>
+          <View style={{ flex: 0.15 }} />
+         
+        </View>
+        <View style={{ flex: 0.45 }}>
+          <View style={styles.TextInput}>
+            <TextInput
+              style={styles.loginBox}
+              placeholder="abc@example.com"
+              placeholderTextColor="gray"
+              keyboardType="email-address"
+              onChangeText={text => {
+                this.setState({
+                  emailId: text
+                });
+              }}
+            />
+            <TextInput
+              style={[styles.loginBox, { marginTop: RFValue(15) }]}
+              secureTextEntry={true}
+              placeholder="Enter Password"
+              placeholderTextColor="gray"
+              onChangeText={text => {
+                this.setState({
+                  password: text
+                });
+              }}
+            />
+          </View>
+          <View style={{ flex: 0.5, alignItems: "center" }}>
+            <TouchableOpacity
+              style={styles.button}
+              onPress={() => {
+                this.userLogin(this.state.emailId, this.state.password);
+              }}
+            >
+              <Text style={styles.buttonText}>Login</Text>
+            </TouchableOpacity>
 
-           <SafeAreaProvider>
-                <AppHeader/>
-                <MyHeader title="Welcome"/>
+            <TouchableOpacity
+              style={styles.button}
+              onPress={() => this.setState({ isModalVisible: true })}
+            >
+              <Text style={styles.buttonText}>SignUp</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
 
-                <View>
-         {this.modal()}
-     </View>
-
-     <View>
-
-     <TextInput style={styles.screenInputs} placeholder="Email ID " keyboardType="email-address"/>
-     <TextInput style={styles.screenInputs} placeholder="password" secureTextEntry={true}/>
-        <TouchableOpacity  onPress={ this.userLogin(this.state.emailId, this.state.password)} style={styles.screenButton}>
-            <Text style={styles.screenButtonText}>
-                Log in
-            </Text>
-        </TouchableOpacity>
-
-          
-        <TouchableOpacity
-           style={styles.button}
-           onPress={()=>this.setState({ isModalVisible:true})}
-           style={styles.screenButton}
-           >
-           <Text style={styles.screenButtonText}>SignUp</Text>
-         </TouchableOpacity>
-
-            </View>
-                <TouchableOpacity 
-                style={styles.button}
-                onPress={()=>{this.props.navigation.navigate("Goals")}}>
-                    <Text>Go</Text>
-                </TouchableOpacity>
-
-                </SafeAreaProvider>
-        )
-    }
-
+        
+      </View>
+    );
+  }
 }
 
-
 const styles = StyleSheet.create({
-    button:{
-        width:50,
-        height:100,
-        borderWidth:4
-    },
-    scrollview: {
-      flex: 1,
-      backgroundColor: "#fff"
-    },
-  signupView: {
-    flex: 0.05,
-    justifyContent: "center",
-    alignItems: "center"
+  container: {
+    flex: 1,
+    backgroundColor: "#6fc0b8"
   },
-  signupText: {
-    fontSize: 20,
-    fontWeight: "bold",
-    color: "#32867d"
+  loginBox: {
+    width: "80%",
+    height: RFValue(50),
+    borderWidth: 1.5,
+    borderColor: "#ffffff",
+    fontSize: RFValue(20),
+    paddingLeft: RFValue(10)
+  },
+  button: {
+    width: "80%",
+    height: RFValue(50),
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: RFValue(25),
+    backgroundColor: "#ffff",
+    shadowColor: "#000",
+    marginBottom: RFValue(10),
+    shadowOffset: {
+      width: 0,
+      height: 8
+    },
+    shadowOpacity: 0.3,
+    shadowRadius: 10.32,
+    elevation: 16
+  },
+  buttonText: {
+    color: "#32867d",
+    fontWeight: "200",
+    fontSize: RFValue(20)
   },
   label: {
-    fontSize: 13,
+    fontSize: RFValue(13),
     color: "#717D7E",
     fontWeight: "bold",
-    paddingLeft: 10,
-    marginLeft: 20
+    paddingLeft: RFValue(10),
+    marginLeft: RFValue(20)
+  },
+  formInput: {
+    width: "90%",
+    height: RFValue(45),
+    padding: RFValue(10),
+    borderWidth: 1,
+    borderRadius: 2,
+    borderColor: "grey",
+    paddingBottom: RFValue(10),
+    marginLeft: RFValue(20),
+    marginBottom: RFValue(14)
   },
   registerButton: {
     width: "75%",
-    height: 50,
-    marginTop:20,
+    height: RFValue(50),
+    marginTop: RFValue(20),
     justifyContent: "center",
     alignItems: "center",
-    borderRadius:3,
+    borderRadius: RFValue(3),
     backgroundColor: "#32867d",
     shadowColor: "#000",
     shadowOffset: {
@@ -282,13 +331,40 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.44,
     shadowRadius: 10.32,
     elevation: 16,
-    marginTop: 10
+    marginTop: RFValue(10)
   },
   registerButtonText: {
-    fontSize: 23,
+    fontSize: RFValue(23),
     fontWeight: "bold",
     color: "#fff"
   },
-})
-
-
+  cancelButtonText: {
+    fontSize: RFValue(20),
+    fontWeight: "bold",
+    color: "#32867d",
+    marginTop: RFValue(10)
+  },
+  scrollview: {
+    flex: 1,
+    backgroundColor: "#fff"
+  },
+  signupView: {
+    flex: 0.05,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  signupText: {
+    fontSize: RFValue(20),
+    fontWeight: "bold",
+    color: "#32867d"
+  },
+  TextInput: {
+    flex: 0.5,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  bookImage: {
+    width: "100%",
+    height: RFValue(220)
+  }
+});
